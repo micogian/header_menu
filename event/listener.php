6915d5258345b7fa93619c2d75d2dd49841d6cf3
micogian <giannidose@tin.it>
2015-07-18 16:35:31 +0200

    Create listener.php

---
+++ event/listener.php
@@ -0,0 +1,32 @@
+<?php
+namespace micogian\header\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class listener implements EventSubscriberInterface
+{
+	protected $template;
+	protected $user;
+
+	public function __construct(\phpbb\template\template $template,\phpbb\user $user)
+	{
+		$this->template = $template; 
+		$this->user = $user;
+	}
+
+	static public function getSubscribedEvents()	
+	{		
+		return array('core.user_setup' => 'setup1',);	
+	}
+	
+	public function setup1($event)	
+	{
+		global $user;
+        if ($this->user->data['group_id'] == 5 || $this->user->data['user_id'] == 2 )
+        {
+		$this->template->assign_vars(array(
+			'STAFF'	=> "staff" )
+			);
+		}
+	}
+}
